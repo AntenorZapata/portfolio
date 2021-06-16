e512a08eabfbbd4895a897c4f3ae734bd5863b37
Antenor <antenorzapata@gmail.com>
2021-06-16 16:57:46 -0300

    project wallet

--- src/data/projetos.jsx
+++ src/data/projetos.jsx
@@ -1,27 +1,26 @@
-import todo from '../imgs/todos.png';
-import port from '../imgs/port.png';
+import wallet from '../imgs/wallet.png';
+// import port from '../imgs/port.png';
 
 const projetos = [
   {
     id: 0,
-    name: 'Portfólio',
-    img: port,
-    desc:
-      'Este site em que você está foi o meu primeiro projeto em React. Nele puder lidar com conceitos essenciais da tecnologia, como State, Props, Hooks, Router, dentre outros. Usei, também, Sass para o estilo, React Icons para os links e o serviço EmailJS para o formulário. React é uma ferramenta incrivel com a qual desejo trabalhar no futuro. :) ',
-    techs: 'React / Sass / EmailJS',
-    linkGit: 'https://github.com/AntenorZapata',
-    linkPage: '',
+    name: 'Wallet',
+    img: wallet,
+    desc:'O app Wallet é uma carteira de controle de gastos com conversor de moedas. Nele é possível adicionar, remover, arquivar e editar um gasto; visualizar os dados da despesa e visualizar o total de gastos – cálculo feito com base no câmbio atual das moedas escolhidas – awesomeAPI de Cotações.',
+    techs: 'React / Redux / Sass',
+    linkGit: 'https://github.com/AntenorZapata/WalletApp',
+    linkPage: 'https://happy-bartik-68a26e.netlify.app/',
   },
-  {
-    id: 1,
-    name: 'Todo-List',
-    img: todo,
-    desc:
-      'Talvez o projeto mais comum na área de desenvolvimento, ainda assim, uma ótima introdução à linguagem (JavaScript). Neste, em especial, implementei diferentes funções: adicionar e apagar uma tarefa; salvar a lista no local storage; marcar tarefas já realizadas; movimentar os itens e apagar a lista. Sobre o estilo, optei por um visual mais clean (minimalista), uma estética que busco sempre em meus projetos.',
-    techs: `HTML / Sass / JavaScript`,
-    linkGit: 'https://github.com/AntenorZapata/minimaList',
-    linkPage: 'https://silly-wozniak-80a536.netlify.app',
-  }, 
+  // {
+  //   id: 1,
+  //   name: 'Todo-List',
+  //   img: todo,
+  //   desc:
+  //     'Talvez o projeto mais comum na área de desenvolvimento, ainda assim, uma ótima introdução à linguagem (JavaScript). Neste, em especial, implementei diferentes funções: adicionar e apagar uma tarefa; salvar a lista no local storage; marcar tarefas já realizadas; movimentar os itens e apagar a lista. Sobre o estilo, optei por um visual mais clean (minimalista), uma estética que busco sempre em meus projetos.',
+  //   techs: `HTML / Sass / JavaScript`,
+  //   linkGit: 'https://github.com/AntenorZapata/minimaList',
+  //   linkPage: 'https://silly-wozniak-80a536.netlify.app',
+  // },
 ];
 
 export default projetos;
