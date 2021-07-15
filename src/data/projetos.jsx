6cbb8afab858ff0bcf5688f60980bbdb48593046
Antenor <antenorzapata@gmail.com>
2021-07-14 21:53:58 -0300

    new project update

--- src/data/projetos.jsx
+++ src/data/projetos.jsx
@@ -1,26 +1,26 @@
 import wallet from '../imgs/wallet.png';
-// import port from '../imgs/port.png';
+import recipes from '../imgs/recipes.png';
 
 const projetos = [
   {
     id: 0,
     name: 'Wallet',
     img: wallet,
-    desc:'O app Wallet é uma carteira de controle de gastos com conversor de moedas. Nele é possível adicionar, remover, arquivar e editar um gasto; visualizar os dados da despesa e visualizar o total de gastos – cálculo feito com base no câmbio atual das moedas escolhidas – awesomeAPI de Cotações.',
+    desc:'O app Wallet é uma carteira de controle de gastos com conversor de moedas. Nele é possível adicionar, remover, arquivar e editar um gasto; visualizar os dados da despesa e visualizar o total de gastos – cálculo feito com base no câmbio atual das moedas escolhidas – awesomeAPI de Cotações. O layout tem como foco dispositivos móveis.',
     techs: 'React / Redux / Sass',
     linkGit: 'https://github.com/AntenorZapata/WalletApp',
-    linkPage: 'https://happy-bartik-68a26e.netlify.app/',
+    linkPage: 'https://reverent-jackson-784b2e.netlify.app/',
   },
-  // {
-  //   id: 1,
-  //   name: 'Todo-List',
-  //   img: todo,
-  //   desc:
-  //     'Talvez o projeto mais comum na área de desenvolvimento, ainda assim, uma ótima introdução à linguagem (JavaScript). Neste, em especial, implementei diferentes funções: adicionar e apagar uma tarefa; salvar a lista no local storage; marcar tarefas já realizadas; movimentar os itens e apagar a lista. Sobre o estilo, optei por um visual mais clean (minimalista), uma estética que busco sempre em meus projetos.',
-  //   techs: `HTML / Sass / JavaScript`,
-  //   linkGit: 'https://github.com/AntenorZapata/minimaList',
-  //   linkPage: 'https://silly-wozniak-80a536.netlify.app',
-  // },
+  {
+    id: 2,
+    name: 'Recipes',
+    img: recipes,
+    desc:
+      'O Recipes é um aplicativo para consulta de receitas de comidas e bebidas. Nele é possível: buscar, filtrar, favoritar e acompanhar o processo de preparação de alimentos e drinks. Utilizei React com foco em Hooks, Context API e componentização. Usei, também, bootstrap para o estilo de alguns itens. A base de dados são 2 (duas) API’s distintas: uma para comidas (https://www.themealdb.com/) e outra para bebidas (https://www.thecocktaildb.com/api.php). O layout tem como foco dispositivos móveis. ',
+    techs: `HTML / Sass / JavaScript`,
+    linkGit: 'https://github.com/AntenorZapata/recipes',
+    linkPage: 'https://github.com/AntenorZapata/recipes',
+  },
 ];
 
 export default projetos;
